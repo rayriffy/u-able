2e63af0758916518e8bfc9301bffe3762ecb381b
Phumrapee Limpianchop <git@rayriffy.com>
2020-04-24 00:53:48 +0700

    feat: intitial c

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import App from 'next/app'
+
+import { Box, CSSReset, ThemeProvider } from '@chakra-ui/core'
+import { css, Global } from '@emotion/core'
+
+class NextApp extends App {
+  public render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <ThemeProvider>
+        <CSSReset />
+        <Global
+          styles={css`
+            html,
+            body,
+            #__next {
+              height: 100%;
+            }
+          `}
+        />
+        <Box height='100%'>
+          <Component {...pageProps} />
+        </Box>
+      </ThemeProvider>
+    )
+  }
+}
+
+export default NextApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Box, Button } from '@chakra-ui/core'
+import { NextPage } from 'next'
+
+const Page: NextPage = (props) => {
+  return (
+    <Box>
+      <Button>OK</Button>
+    </Box>
+  )
+}
+
+export default Page
